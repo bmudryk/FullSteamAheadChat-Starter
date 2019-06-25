dcee2d9a996ee088c8a3eb99feb0b62d1a5c0ea4
Brenda L Mudryk <131078@carmax.com>
2019-06-24 22:29:28 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const port = process.env.PORT || 3000
+
+var app = require('express')();
+var server = require('http').Server(app);
+
+//  Add step 3 right below this line
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//  Add step 2 right below this line
+
+
+//  Add step 4 right below this line
+
+server.listen(port, () => {
+    console.log('Server is running on port:  ', server.address().port);
+});
